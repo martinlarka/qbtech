0143d1dd22375efec31839a3662ba361e35148e4
Martin Lärka <martin.larka@gmail.com>
2024-02-20 16:57:09 +0100

    Take text input name from control

--- src/components/TextInput.tsx
+++ src/components/TextInput.tsx
@@ -37,7 +37,6 @@ export const TextInput = ({
       </label>
       <input
         type="text"
-        name={control.name}
         className={twMerge(
           "border text-sm rounded-lg block w-full p-2.5 dark:bg-gray-700",
           "bg-gray-50  border-gray-300 text-gray-900 focus:ring-blue-500 focus:border-blue-500 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500",
